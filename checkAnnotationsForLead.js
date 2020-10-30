a92ef4b75aca186ed5366ada65243e565ea125b3
highzor <52636901+highzor@users.noreply.github.com>
2020-10-30 15:16:36 +0300

    Create checkAnnotationsForLead.js

---
+++ checkAnnotationsForLead.js
@@ -0,0 +1,28 @@
+function checkAnnotationsForLead() {
+    var entityId = Xrm.Page.data.entity.getId().replace(/\{|\}/g, '');
+    var oDataEndpointUrl = "http://10.40.10.146/LearnAPetukhov/api/data/v9.0/leads(" + entityId + ")/Lead_Annotation?$count=true";
+    var service = GetRequestObject();
+    if (service != null) {
+        service.open("GET", oDataEndpointUrl, false);
+        service.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        service.setRequestHeader("Accept", "application/json, text/javascript, */*");
+        service.send(null);
+        var retrieved = JSON.parse(service.responseText);
+        if (retrieved["@odata.count"] > 0) {
+            Xrm.Page.ui.tabs.get("tab_3").setDisplayState("expanded");
+        } else {
+            Xrm.Page.ui.tabs.get("tab_3").setDisplayState("collapsed");
+        }
+    }
+}
+function GetRequestObject() {
+    if (window.XMLHttpRequest) {
+        return new window.XMLHttpRequest;
+    } else {
+        try {
+            return new ActiveXObject("MSXML2.XMLHTTP.3.0");
+        } catch (ex) {
+            return null;
+        }
+    }
+}
