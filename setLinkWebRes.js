0b885438f980e6554e580bfeb0c20b60768e0e32
highzor <52636901+highzor@users.noreply.github.com>
2020-10-30 22:06:40 +0300

    Update setLinkWebRes.js

--- setLinkWebRes.js
+++ setLinkWebRes.js
@@ -1,9 +1,13 @@
 function setLinkWebRes() {
     if (Xrm.Page.ui.getFormType() != 1) {
+		Xrm.Page.ui.tabs.get("tab_3").setVisible(true)
 		Xrm.Page.getControl("WebResource_myHtml").setVisible(true);
         var link = Xrm.Page.getControl("WebResource_myHtml").getSrc();
         var entityId = Xrm.Page.data.entity.getId().replace(/\{|\}/g, '');
         link += "?Data=" + entityId;
         Xrm.Page.getControl("WebResource_myHtml").setSrc(link);
-    }
+    } else {
+		Xrm.Page.ui.tabs.get("tab_3").setVisible(false)
+		Xrm.Page.getControl("WebResource_myHtml").setVisible(false);
+	}
 }
